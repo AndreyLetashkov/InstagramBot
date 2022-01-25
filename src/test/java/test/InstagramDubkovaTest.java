556ebb5238e6ba4f43d80ea60912f2e73d153a42
Andrew <a.letashkou@godeltech.com>
2022-01-26 02:25:25 +0300

    Add

--- src/test/java/test/InstagramDubkovaTest.java
+++ src/test/java/test/InstagramDubkovaTest.java
@@ -4,6 +4,7 @@ import helper.BaseTest;
 import org.testng.annotations.Test;
 import utils.DriverUtils;
 import utils.ParserUtils;
+import utils.RandomUtils;
 
 public class InstagramDubkovaTest extends BaseTest {
     /**
@@ -30,18 +31,9 @@ public class InstagramDubkovaTest extends BaseTest {
     @Test
     public void subscribeTest() {
         loginPage.login(ParserUtils.parseConfig("USERNAME_DUBKOVA"), ParserUtils.parseConfig("PASSWORD_DUBKOVA"));
-        DriverUtils.navigate(ParserUtils.parseTestData("nanosy_otdyh"));
+        DriverUtils.navigate(ParserUtils.parseTestData(RandomUtils.getRandomNumber()));
         searchPage.clickFollowersHref();
         followersForm.scrollDown(50);
         followersForm.subscribe(50);
-    }
-
-    @Test
-    public void unsubscribeTest() {
-        loginPage.login(ParserUtils.parseConfig("USERNAME_ANDREY"), ParserUtils.parseConfig("PASSWORD_ANDREY"));
-        DriverUtils.navigate(ParserUtils.parsePath(""));
-        homePage.clickFollowingBtn();
-        followingForm.scrollDown(50);
-        followingForm.unSubscribe(50);
     }
 }
