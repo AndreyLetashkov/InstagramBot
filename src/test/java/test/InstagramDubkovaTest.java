acf5c7a21ab8122d8716861e56fd400127ef90cf
andrei.letashkou <letashkov19981@mail.ru>
2022-03-30 01:58:11 +0300

    Update

--- src/test/java/test/InstagramDubkovaTest.java
+++ src/test/java/test/InstagramDubkovaTest.java
@@ -7,27 +7,7 @@ import utils.ParserUtils;
 import utils.RandomUtils;
 
 public class InstagramDubkovaTest extends BaseTest {
-    /**
-     * zdorovei_naroch
-     * nanosy_otdyh
-     * usadby_by
-     * dvor_bel
-     * agrousadba_naturlich
-     * am555_by
-     * eco_ysadba
-     * routebelarus
-     * <p>
-     * tutbylive
-     * minsk_news
-     * minsk_online24
-     * minskchp
-     * xxminsk
-     * belarus_top
-     * minsk_novostnoy
-     * gbminsk
-     * minsk
-     * news_time_minsk
-     */
+
     @Test
     public void subscribeTest() {
         loginPage.login(ParserUtils.parseConfig("USERNAME_DUBKOVA"), ParserUtils.parseConfig("PASSWORD_DUBKOVA"));
