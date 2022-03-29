4f386c0e41f94631094edb6bec9060aaac15377b
andrei.letashkou <letashkov19981@mail.ru>
2022-03-30 01:59:56 +0300

    Update

--- src/test/java/test/InstagramVikaTest.java
+++ src/test/java/test/InstagramVikaTest.java
@@ -7,28 +7,6 @@ import utils.ParserUtils;
 import utils.RandomUtils;
 
 public class InstagramVikaTest extends BaseTest {
-
-    /**
-     * community:
-     * myataBrest
-     * loft_lounge_bar_brest
-     * onlinebrestby
-     * brestcity_news
-     * brestIlovepublic
-     * virtualbrest
-     * wowbrest
-     * <p>
-     * tutbylive
-     * minsk_news
-     * minsk_online24
-     * minskchp
-     * xxminsk
-     * belarus_top
-     * minsk_novostnoy
-     * gbminsk
-     * minsk
-     * news_time_minsk
-     */
     @Test
     public void subscribeTest() {
         loginPage.login(ParserUtils.parseConfig("USERNAME_VIKA"), ParserUtils.parseConfig("PASSWORD_VIKA"));
